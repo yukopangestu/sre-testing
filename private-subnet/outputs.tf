85396921a6e7cd991bb174b8a20a1a5d325bea99
yuko pangestu <yuko.pangestu@gmail.com>
2026-09-25 13:28:28 +0700

    chore: remove private subnet module

--- private-subnet/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "subnet_id" {
-  description = "ID of the private subnet."
-  value       = aws_subnet.private.id
-}
-
-output "route_table_id" {
-  description = "ID of the private route table."
-  value       = aws_route_table.private.id
-}
